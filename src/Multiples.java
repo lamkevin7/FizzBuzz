6a2915bd8dc3a0cad0edc39efed6a5be77d24990
Kevin <kevv.lam@mail.utoronto.ca>
2023-09-11 10:47:13 -0400

    print out the number of multiples of 3 and 5

---
+++ src/Multiples.java
@@ -0,0 +1,8 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int multiplesOf5 = 999 / 5;
+        int multiplesOf3 = 999 / 3;
+        int multiplesOf3And5 = multiplesOf3 + multiplesOf5;
+        System.out.println(multiplesOf3And5);
+    }
+}
